12498219a20d66d3b34f63c4fbb9431f36404362
sumit8080 <summit@pariltech.com>
2024-09-15 17:33:24 +0400

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,148 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg'>
+      <h1 className='text-4xl font-bold mb-4'>Privacy Policy</h1>
+      <p className='text-sm text-gray-500 mb-6'>
+        Effective Date: 15 September 2024
+      </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>1. Introduction</h2>
+        <p className='text-gray-700 mb-4'>
+          Farm Price 24 we operates the Farm Price 24 mobile application (the
+          "App"). This Privacy Policy explains how we collect, use, and share
+          information when you use our App. We are committed to protecting your
+          privacy and ensuring that your personal information is handled in a
+          safe and responsible manner.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>
+          2. Information We Collect
+        </h2>
+        <p className='text-gray-700 mb-4'>
+          We collect the following types of information:
+        </p>
+
+        <h3 className='text-lg font-semibold mb-2'>a. Personal Information</h3>
+        <p className='text-gray-700 mb-4'>
+          We do not collect personal information such as your name, email
+          address, or contact information unless you voluntarily provide it, for
+          example, when you contact us for support.
+        </p>
+
+        <h3 className='text-lg font-semibold mb-2'>
+          b. Non-Personal Information
+        </h3>
+        <p className='text-gray-700 mb-4'>
+          We collect non-personal data such as device information (e.g., device
+          model, operating system version), usage data (e.g., pages visited,
+          actions taken within the app), and general geographic information
+          (e.g., city or country).
+        </p>
+
+        <h3 className='text-lg font-semibold mb-2'>
+          c. Cookies and Tracking Technologies
+        </h3>
+        <p className='text-gray-700 mb-4'>
+          We may use cookies and similar tracking technologies to analyze user
+          behavior and improve the performance of our App.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>
+          3. How We Use Your Information
+        </h2>
+        <p className='text-gray-700 mb-4'>
+          We use the information we collect for the following purposes:
+        </p>
+        <ul className='list-disc list-inside text-gray-700 mb-4'>
+          <li>To provide and improve the functionality of the App.</li>
+          <li>To analyze trends, usage, and activities related to the App.</li>
+          <li>To respond to user inquiries and provide customer support.</li>
+        </ul>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>
+          4. Sharing Your Information
+        </h2>
+        <p className='text-gray-700 mb-4'>
+          We do not sell, trade, or otherwise transfer your personal information
+          to third parties, except in the following cases:
+        </p>
+        <ul className='list-disc list-inside text-gray-700 mb-4'>
+          <li>
+            <strong>Service Providers:</strong> We may share your information
+            with trusted service providers who assist us in operating our App or
+            conducting our business.
+          </li>
+          <li>
+            <strong>Legal Requirements:</strong> We may disclose your
+            information to comply with legal obligations or protect the rights,
+            property, or safety of Farm Price 24, our users, or others.
+          </li>
+        </ul>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>5. Data Security</h2>
+        <p className='text-gray-700 mb-4'>
+          We take reasonable measures to protect the information we collect from
+          unauthorized access, use, or disclosure. However, please be aware that
+          no security measures are perfect or impenetrable.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>6. Data Retention</h2>
+        <p className='text-gray-700 mb-4'>
+          We retain non-personal information for as long as necessary to fulfill
+          the purposes outlined in this Privacy Policy or as required by law.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>7. Your Choices</h2>
+        <p className='text-gray-700 mb-4'>
+          You can choose not to provide personal information, although this may
+          limit your ability to use certain features of the App. You can also
+          manage cookies and tracking technologies through your device or
+          browser settings.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>8. Children's Privacy</h2>
+        <p className='text-gray-700 mb-4'>
+          Our App is not intended for children under the age of 13, and we do
+          not knowingly collect personal information from children.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>
+          9. Changes to This Privacy Policy
+        </h2>
+        <p className='text-gray-700 mb-4'>
+          We may update this Privacy Policy from time to time. Any changes will
+          be posted on this page, and the "Effective Date" will be updated
+          accordingly.
+        </p>
+      </section>
+
+      <section className='mb-6'>
+        <h2 className='text-2xl font-semibold mb-2'>10. Contact Us</h2>
+        <p className='text-gray-700 mb-4'>
+          If you have any questions or concerns about this Privacy Policy,
+          please contact us at:
+        </p>
+        <address className='text-gray-700 mb-4'>
+          farmprice24@gmail.com <br />
+        </address>
+      </section>
     </div>
   );
 }
